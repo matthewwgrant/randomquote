58742cea82066bd4d9ea46ee7909fa5b23760c6e
Matthew Grant <amattwgrant@gmail.com>
2021-09-09 16:57:02 -0400

    Update script.js
    Updated code comments

--- js/script.js
+++ js/script.js
@@ -3,37 +3,33 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ Array of quote
 ***/
 const quotes = [
 	{quote: `To be or not to be, that is the question`, source: `William Shakespeare`, citation: `Hamlet`, year: 1609},
 	{quote: `You miss 100% of the shots that you don't take - Wayne Gretzky`, source: `Michael Scott`, citation: `The Office`, year: 2006},
 	{quote: `Ooh wee ooh I look just like Buddy Holly`, source: `Weezer`, citation: `Blue Album`, year: 1994},
-	{quote: `When you see 50 guys that look like you fighting over a shirt, you go in there. Yes you do.`, source: `Tim Robinson`, citation: `I Think You Should Leave`, year: 2021},
+	{quote: `When you walk by a store and see 50 guys that look like you fighting over a shirt, you go in there. Yes you do.`, source: `Tim Robinson`, citation: `I Think You Should Leave`, year: 2021},
 	{quote: `When you come to a fork in the road, take it.`, source: `Yogi Berra`, citation: `The Yogi Book: I Really Didn’t Say Everything I Said!`, year: 1988},
-	// {quote: ``, author: ``},
-	// {quote: ``, author: ``}
+	// {quote: ``, author: ``, citation: ``, year: },
+	// {quote: ``, author: ``, citation: ``, year: }
 ];
 
 
 /***
- * `changeBackground` function
  Generates a random number between 0-255 that
  is returned and used to change the background
  color of the body when printQuote is called
  by being inserted as numbers for rgb color
 ***/
+
 function changeBackground() {
 	return Math.floor(Math.random() * 256);
 }
 
 /***
- * `getRandomQuote` function
  Uses a random number to generate a number between 0, or
  the start of the array, to the last index of the array. No
  1 is added to this like a dice game because the index starts 
@@ -46,13 +42,13 @@ function getRandomQuote() {
 
 
 /***
- * `printQuote` function
  Uses the numbers from randomBackground to set the rgb color.
  It then stores the call to getRandomQuote in a variable that is 
  used with the prop name to get the information needed to display the
  quote, which is stored in a variable quote, made up of template literala
  and HTML. This is returned and then displayed.
 ***/
+
 function printQuote() {
 	document.querySelector('body').style.background = `rgb(${changeBackground()},${changeBackground()},${changeBackground()})`
 	let randomQuote = getRandomQuote();
@@ -65,6 +61,12 @@ function printQuote() {
 	return document.querySelector('.container').innerHTML = quote;
 }
 
+/** 
+	Calls printQuote every 5 seconds even if the
+	click event is not called
+**/
+
+setInterval(printQuote, 5000);
 
 /** 
 	Calls printQuote as soon as the page has loaded
