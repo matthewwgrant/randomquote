75d85acbdaa974d14cfc1b59d3824176bf5ecbdb
Matthew Grant <amattwgrant@gmail.com>
2021-09-09 20:03:01 -0400

    Finished
    Finished extra credit properly

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ project 1 - A Random Quote Generator
  Array of quote
 ***/
 const quotes = [
-	{quote: `To be or not to be, that is the question`, source: `William Shakespeare`, citation: `Hamlet`, year: 1609},
+	{quote: `To be or not to be, that is the question`, source: `William Shakespeare`, year: 1609},
 	{quote: `You miss 100% of the shots that you don't take - Wayne Gretzky`, source: `Michael Scott`, citation: `The Office`},
-	{quote: `Ooh wee ooh I look just like Buddy Holly`, source: `Weezer`, year: 1994},
-	{quote: `When you walk by a store and see 50 guys that look like you fighting over a shirt, you go in there. Yes you do.`, source: `Tim Robinson`, citation: `I Think You Should Leave`, year: 2021},
-	{quote: `When you come to a fork in the road, take it.`, source: `Yogi Berra`},
-	{quote: `There’s only one thing I hate more than lying: skim milk. Which is water that is lying about being milk.`, source: `Ron Swanson`, citation: `Parks and Recreation`, year: 2014}
+	{quote: `Ooh wee ooh I look just like Buddy Holly`, source: `Weezer`, category: `Music`},
+	{quote: `When you walk by a store and see 50 guys that look like you fighting over a shirt, you go in there. Yes you do.`, source: `Tim Robinson`, category: 'Humor'},
+	{quote: `When you come to a fork in the road, take it.`, source: `Yogi Berra`, category: `Sports`},
+	{quote: `There’s only one thing I hate more than lying: skim milk. Which is water that is lying about being milk.`, source: `Ron Swanson`, citation: `Parks and Recreation`}
 ];
 
 
@@ -52,33 +52,33 @@ function printQuote() {
 	document.querySelector('body').style.background = `rgb(${changeBackground()},${changeBackground()},${changeBackground()})`
 	let randomQuote = getRandomQuote();
 	let quote = `
-		<div id="quote-box" class="quote-box">
-        	<p class="quote">${randomQuote.quote}</p>
-        	<p class="source">${randomQuote.source}
+    	<p class="quote">${randomQuote.quote}</p>
+    	<p class="source">${randomQuote.source}
 		`;
-	if ( 'citation' in randomQuote && 'year' in randomQuote ) {
-		quote += `
-			<span class="citation">${randomQuote.citation}</span><span class="year">${randomQuote.year}</span></p>
-				</div>`;
-		} else if ( 'citation' in randomQuote ) {
-			quote += `
-			<span class="citation">${randomQuote.citation}</span></p>
-				</div>`;
-		} else if ( 'year' in randomQuote ) {
-			quote += `<span class="year">${randomQuote.year}</span></p>
-				</div`;
-		} else {
-			quote += `</p>
-				</div>`;
-		}
 
-	return document.querySelector('.container').innerHTML = quote;
+	if ( 'year' in randomQuote ) {
+		quote += `<span class="year">${randomQuote.year}</span>`;
+	}
+
+	if ( 'citation' in randomQuote ) {
+		quote += `
+		<span class="citation">${randomQuote.citation}</span>`
+	}
+
+	if ( 'category' in randomQuote ) {
+		quote += `
+		<span class="citation">${randomQuote.category}</span>`
+	}
+
+	quote += `</p>`;
+
+	return document.getElementById('quote-box').innerHTML = quote;
 }
 
 
 /** 
 	Calls printQuote every 5 seconds even if the
-	click event is not called
+	click event is not called.
 **/
 
 setInterval(printQuote, 5000);
